ff4b445f633b6dd258c2d7ed2173b83aa66a746f
William Pilger <40075505+williampilger@users.noreply.github.com>
2021-08-10 08:41:29 -0300

    Create Instalar-MS_SQL_Server-Ubuntu20.04.sh

---
+++ sql/Instalar-MS_SQL_Server-Ubuntu20.04.sh
@@ -0,0 +1,19 @@
+sudo su
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
+sudo apt-get update
+sudo apt-get install -y mssql-server
+sudo /opt/mssql/bin/mssql-conf setup
+
+sudo apt install curl
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+
+sudo apt-get install mssql-tools unixodbc-dev
+sudo apt-get install mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+
+#sqlcmd -S localhost -U SA -P '<YourPassword>' #Use isso para Conectar ao seu servidor
+#Retirado de: https://docs.microsoft.com/pt-br/sql/linux/quickstart-install-connect-ubuntu?view=sql-server-ver15
