64aab59b777bf28b17a47ed3b16d19acab9972b0
William Pilger <40075505+williampilger@users.noreply.github.com>
2021-08-10 09:32:26 -0300

    Update Instalar-MS_SQL_Server-Ubuntu20.04.sh

--- sql/Instalar-MS_SQL_Server-Ubuntu20.04.sh
+++ sql/Instalar-MS_SQL_Server-Ubuntu20.04.sh
@@ -1,4 +1,5 @@
-sudo su
+#FALHOU
+#sudo su
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
 sudo apt-get update
