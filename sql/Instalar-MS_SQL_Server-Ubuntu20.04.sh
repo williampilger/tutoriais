1f61b7b61520cd573fc5ee8a0fc8f84946e6aead
William Pilger <40075505+williampilger@users.noreply.github.com>
2021-08-10 09:24:31 -0300

    Update Instalar-MS_SQL_Server-Ubuntu20.04.sh

--- sql/Instalar-MS_SQL_Server-Ubuntu20.04.sh
+++ sql/Instalar-MS_SQL_Server-Ubuntu20.04.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install -f -y mssql-server
 sudo /opt/mssql/bin/mssql-conf setup
 
-sudo apt install curl
+sudo apt install -f -y curl
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 
