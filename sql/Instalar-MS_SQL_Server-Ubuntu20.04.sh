7a62a55aaed9d7617d0c69594d85176d7f463d44
William Pilger <40075505+williampilger@users.noreply.github.com>
2021-08-10 09:24:04 -0300

    Update Instalar-MS_SQL_Server-Ubuntu20.04.sh

--- sql/Instalar-MS_SQL_Server-Ubuntu20.04.sh
+++ sql/Instalar-MS_SQL_Server-Ubuntu20.04.sh
@@ -2,15 +2,15 @@ sudo su
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
 sudo apt-get update
-sudo apt-get install -y mssql-server
+sudo apt-get install -f -y mssql-server
 sudo /opt/mssql/bin/mssql-conf setup
 
 sudo apt install curl
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
 
-sudo apt-get install mssql-tools unixodbc-dev
-sudo apt-get install mssql-tools
+sudo apt-get install -f -y mssql-tools unixodbc-dev
+sudo apt-get install -f -y mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
